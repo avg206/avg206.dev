c1e1a15a826756201c325ac9b0970415458c79df
Anatoli Huseu <anatolihuseu@gmail.com>
2024-12-30 18:57:10 +0100

    feat: prepare first minimal version of content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['"JetBrains Mono"', 'Inter', 'sans-serif'],
+      serif: ['"JetBrains Mono"', 'Merriweather', 'serif'],
+      mono: ['"JetBrains Mono"', 'monospace'],
+    },
+    colors: {
+      primary: '#1c1917',
+      secondary: '#4B5563',
+    }
+  },
+  plugins: [],
+};
