101f3067b6b504d134ec91912e725de2443cc4eb
Anatoli Huseu <anatolihuseu@gmail.com>
2024-12-30 21:30:49 +0100

    ci: setup ghpages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://avg206.dev',
+  base: '',
   integrations: [mdx(), sitemap(), tailwind()],
 });
